4e8ba3b81525be69adf12f08e8c62c9080299532
Grootabs <abs122516@gmail.com>
2022-08-11 14:52:46 -0500

    Finished Exercise

---
+++ app.js
@@ -0,0 +1,24 @@
+const firstName = "Aiden";
+let lastName = "Smith";
+var age = 18;
+
+let language = "Javascript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(title)
+console.log(language)
+console.log(stock)
